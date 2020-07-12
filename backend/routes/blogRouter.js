a35f19e42cbb7f391162bbebc7c03d50a3e90bda
VedantBang <vedantbang5@gmail.com>
2020-07-12 08:47:10 +0530

    blog router

--- backend/routes/blogRouter.js
+++ backend/routes/blogRouter.js
@@ -5,22 +5,26 @@ const upload = multer({ dest:'../assets/' });
 const cut = require('../utilities/cut');
 const Blog = require('../models/blog');
 
-router.post('/addsimpleblog', upload.none(), async (req,res,next) => {
+
+router.post('/addblogimg', upload.single('image'), async (req,res,next) => {
 	try{
 		let input = cut(req.body, ['userid','label','title','body']);
-		let simpleBlog = new Blog(input);
-		await simpleBlog.save();
+		input.image = req.file.path;
+		let newBlog = new Blog(input);
+		await newBlog.save();
 		res.status(200).json({});
 	} catch(err){ next(err); }
 });
 
-router.post('/addimg', upload.single('picture'), async (req,res,next) => {
+
+router.post('/addblogvid', upload.single('video'), async (req,res,next) => {
 	try{
-		let img = cut(req.body, ['image']);
-		let encode_image = img.toString('base64');
-		await encode_image.save();
+		let input = cut(req.body, ['userid','label','title']);
+		input.video = req.file.path;
+		let newBlog = new Blog(input);
+		await newBlog.save();
 		res.status(200).json({});
 	} catch(err){ next(err); }
-})
+});
 
 module.exports = router;
