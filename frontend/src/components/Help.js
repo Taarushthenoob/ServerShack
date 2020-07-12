38b51952b4b633d81819019f67066eb295962cbf
Taarushthenoob <54764915+Taarushthenoob@users.noreply.github.com>
2020-07-12 20:17:24 +0530

    made some changes in help section

--- frontend/src/components/Help.js
+++ frontend/src/components/Help.js
@@ -6,7 +6,6 @@ import './Help.css';
 const Help = () => {
   return (
     <div>
-<<<<<<< HEAD
         <div className="cont">
             <h1>Want to Provide Your Invaluable Help?</h1>
             <OrgList />
@@ -20,20 +19,6 @@ const Help = () => {
             <h1>Do you Need Help?</h1>
             <Helpers />
         </div>
-=======
-      <div className="cont">
-        <h1>Provide Help</h1>
-        <OrgList />
-      </div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <div className="cont">
-        <h1>Need Help</h1>
-        <Helpers />
-      </div>
->>>>>>> 925647f125f3fc3509885c15bfd9a81591d209ed
     </div>
   );
 };
