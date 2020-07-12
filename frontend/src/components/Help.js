4d6c8977ed7338d1b7e85024d3df9b0f9287f87b
Rohit-Mundada <rohitrm0801@gmail.com>
2020-07-12 20:32:52 +0530

    Updates

--- frontend/src/components/Help.js
+++ frontend/src/components/Help.js
@@ -5,22 +5,7 @@ import './Help.css';
 
 const Help = () => {
   return (
-    <div>
-<<<<<<< HEAD
-        <div className="cont">
-            <h1>Want to Provide Your Invaluable Help?</h1>
-            <OrgList />
-        </div>
-        <br /><br />
-        <div class="ui horizontal divider">
-            Or
-        </div>
-        <br /><br />
-        <div className="cont">
-            <h1>Do you Need Help?</h1>
-            <Helpers />
-        </div>
-=======
+    <div style={{ marginTop: '2rem'}}>
       <div className="cont">
         <h1>Provide Help</h1>
         <OrgList />
@@ -33,7 +18,6 @@ const Help = () => {
         <h1>Need Help</h1>
         <Helpers />
       </div>
->>>>>>> 9facd48e1b42da34f4bc655ff5db8ab96638e9fd
     </div>
   );
 };
